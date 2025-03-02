f6eca6b5c766a24c4de0c5fb9654f3c6f53910d9
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-01 23:59:33 -0300

    feat: componente de card dos livros por genero criado

---
+++ src/components/CardGenero/index.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from 'react-router-dom';
+import CardLivro from '../CardLivro'
+import style from './styles.module.css'
+
+interface Livro {
+    id: number;
+    titulo: string;
+    autor: string;
+    genero: string;
+    preco: number;
+    capa: string;
+}
+
+interface CardGeneroProps {
+    genero: string;
+    livros: Livro[];
+}
+
+export default function CardGenero({ genero, livros }: CardGeneroProps) {
+
+    const navigate = useNavigate()
+
+    function handleClick() {
+        navigate("/home/genre")
+    }
+
+    const livrosFiltrados = livros.filter(livro => livro.genero === genero).slice(0,4)
+
+    return (
+        <>
+            <div className={style.card}>
+                <div className={style.about}>
+                   <h1>{genero}</h1>
+                    <p onClick={handleClick}>Ver mais</p> 
+                </div>
+                <div className={style.livros}>
+                    {livrosFiltrados.map(livro => (
+                        <CardLivro key={livro.id} livro={livro}/>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
