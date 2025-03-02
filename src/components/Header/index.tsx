0f5c4bd2e3a05d8e377f6b25de94192b7a020c42
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-01 23:54:02 -0300

    fix: imagens do componente header adicionados

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,32 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styles from './styles.module.css'
+import logo from '../../../public/logo.png'
+import profile from '../../assets/Profile.png'
+import cart from '../../assets/Shop.png'
 
 export default function Header() {
+    const navigate = useNavigate();
+
+    function navigateToHome() {
+        navigate("/home");
+    }
+    function navigateToLogin() {
+        navigate("/");
+    }
     return (
         <>
-            <button>
-                <Link to="/home/genre">
-                Genre
-                </Link>
-            </button>
-            <div>
-            <button>
-                <Link to="/home/details">
-                Details
-                </Link>
-            </button>
-            <button>
-                <Link to="/">
-                Login
-                </Link>
-            </button>
+            <div className={styles.box}>
+                <button onClick={navigateToHome}>
+                    <img src={logo} alt="logo da livraria" />
+                </button>
+                <div className={styles.secondaryButtons}>
+                <button onClick={navigateToLogin}>
+                    <img src={profile} alt="imagem de profile" />
+                </button>
+                <button>
+                    <img src={cart} alt="" />
+                </button>
+                </div>
             </div>
         </>
     )
