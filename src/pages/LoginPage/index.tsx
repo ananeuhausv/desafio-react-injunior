e5d7369aae08ab13aebcda5b059cbef18a424d8f
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-01 23:55:17 -0300

    feat: login page criada

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import login from '../../assets/login-background.png'
+import logo from '../../../public/logo.png'
+import style from './styles.module.css'
+import LoginInput from '../../components/LoginInput'
 
 export default function LoginPage() {
+    
+
     return(
         <>
-        <button>
-            <Link to="/Home">Log in</Link>
-        </button>
+            <div className={style.box}>
+                <div className={style.background}>
+                <img src={login} alt="foto de livros e uma luminária"/> 
+                </div>
+                <div className={style.login}>
+                    <img src={logo} alt="logo" />
+
+                    <p>Bem vindo(a)!</p>
+                    <h2>Entre na sua conta</h2>
+
+                    <LoginInput/>
+                    
+                </div>
+            </div>
         </>
     )
 }
