6c794cf7b6860e9bb0501b8db8a275d78e412027
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-02 00:01:34 -0300

    feat: página de detalhes do livro criada

--- src/pages/BookDetailsPage/index.tsx
+++ src/pages/BookDetailsPage/index.tsx
@@ -1,6 +1,41 @@
-export default function BookDetailsPage() {
-    return(
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+interface Livro {
+    id: number;
+    titulo: string;
+    autor: string;
+    preco: number;
+    capa: string;
+    sinopse: string;
+}
+
+export default function BookDetails() {
+    const { id } = useParams(); 
+    const [livro, setLivro] = useState<Livro | null>(null)
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/livros/${id}`)
+            .then(response => setLivro(response.data))
+            .catch(error => console.error('Algo deu errado: ' + error))
+    },[])
+
+    return (
         <>
+        <div>
+            <h1>Detalhes do Livro {id}</h1>
+        </div>
+        <div>
+            <img src={livro?.capa} alt="capa do livro" />
+        </div>
+        <div>
+            <h1>{livro?.titulo}</h1>
+            <h3>{livro?.autor}</h3>
+
+            <p>{livro?.sinopse}</p>
+        </div>
+        <button>{livro?.preco} Adicionar ao carrinho</button>
         </>
-    )
+    );
 }
