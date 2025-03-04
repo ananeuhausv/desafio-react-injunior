a4b992d551ad10649e2529855809ca6dd47b7b4b
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-03-03 22:32:55 -0300

    rota para paǵina de gênero adicionada

--- src/router.tsx
+++ src/router.tsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
                 element: <Home/>
             },
             {
-                path: "genre",
+                path: "genre/:genero",
                 element: <Genre/>
             },
             {
